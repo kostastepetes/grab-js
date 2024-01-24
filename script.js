ade7ce9931bf032381458bd87660d4c267e2a0d6
Kostas Tepetes <62772104+kostastepetes@users.noreply.github.com>
2024-01-24 10:16:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,113 +1,116 @@
 const grab = {
-    targetElement: null,
-    callback: null,
-    method: null,
-    url: null,
-    async: true,
-    xhr: new XMLHttpRequest(),
+  targetElement: null,
+  callback: null,
+  method: null,
+  url: null,
+  async: true,
+  xhr: new XMLHttpRequest(),
+
+  perform: function(method, url, async, enableCors) {
+    this.method = method;
+    this.url = url;
+    this.async = async;
+    this.xhr = new XMLHttpRequest();
+    this.cors(enableCors || false);
+    return this;
+   },
+   
+   cors: function(enable) {
+    this.xhr.withCredentials = enable;
+    return this;
+   },
+
+  here: function(targetElement, responseType, headers) {
+    this.targetElement = document.getElementById(targetElement);
+    this.callback = responseType;
+    this.headers = headers;
+    return this;
+  },
   
-    perform: function(method, url, async, enableCors) {
-      this.method = method;
-      this.url = url;
-      this.async = async;
-      this.xhr = new XMLHttpRequest();
-      this.cors(enableCors || false);
-      return this;
-     },
-     
-     cors: function(enable) {
-      this.xhr.withCredentials = enable;
-      return this;
-     },
+  setData: function(data) {
+    this.data = data;
+    return this;
+  },
+
+  error: function(callback) {
+    this.xhr.onerror = callback;
+    return this;
+  },
+
+  timeout: function(milliseconds, callback) {
+    this.xhr.timeout = milliseconds;
+    this.xhr.ontimeout = callback;
+    return this;
+  },
+
+  progress: function(callback) {
+    this.xhr.onprogress = callback;
+    return this;
+  },
+
+  responseType: function(type) {
+    this.xhr.responseType = type;
+    return this;
+  },
+
+  abort: function() {
+    this.xhr.abort();
+    console.log('Request aborted.');
+    // Handle abortion appropriately
+    return this;
+  },
+
+  cache: function(value) {
+    this.xhr.setRequestHeader('Cache-Control', value);
+    return this;
+  },
+
+  done: function () {
+      const self = this;
+      this.xhr.onload = function () {
+          self.handleResponse();
+      };
   
-    here: function(targetElement, responseType, headers) {
-      this.targetElement = document.getElementById(targetElement);
-      this.callback = responseType;
-      this.headers = headers;
-      return this;
-    },
-    
-    setData: function(data) {
-      this.data = data;
-      return this;
-    },
+      this.xhr.open(this.method, this.url, this.async);
   
-    error: function(callback) {
-      this.xhr.onerror = callback;
-      return this;
-    },
+      if (this.xhr.withCredentials) {
+          console.log('CORS is enabled.');
+      } else {
+          console.log('CORS is disabled.');
+      }
   
-    timeout: function(milliseconds, callback) {
-      this.xhr.timeout = milliseconds;
-      this.xhr.ontimeout = callback;
-      return this;
-    },
+      if (this.headers) {
+          // Set custom headers if provided
+          for (const [headerName, headerValue] of Object.entries(this.headers)) {
+              this.xhr.setRequestHeader(headerName, headerValue);
+          }
+      }
   
-    progress: function(callback) {
-      this.xhr.onprogress = callback;
-      return this;
-    },
+      if (this.method.toUpperCase() === 'POST') {
+          if (!this.headers || !this.headers['Content-Type']) {
+              console.error('Content type is not specified for POST request.');
+              return;
+          }
+      }
   
-    responseType: function(type) {
-      this.xhr.responseType = type;
-      return this;
-    },
+      this.xhr.send(this.data);
+  },
   
-    abort: function() {
-      this.xhr.abort();
-      console.log('Request aborted.');
-      // Handle abortion appropriately
-      return this;
-    },
-  
-    cache: function(value) {
-      this.xhr.setRequestHeader('Cache-Control', value);
-      return this;
-    },
-  
-    done: function () {
-        const self = this;
-        this.xhr.onload = function () {
-            self.handleResponse();
-        };
-    
-        this.xhr.open(this.method, this.url, this.async);
-    
-        if (this.xhr.withCredentials) {
-            console.log('CORS is enabled.');
-        } else {
-            console.log('CORS is disabled.');
-        }
-    
-        if (this.headers) {
-            // Set custom headers if provided
-            for (const [headerName, headerValue] of Object.entries(this.headers)) {
-                this.xhr.setRequestHeader(headerName, headerValue);
-            }
-        }
-    
-        if (this.method.toUpperCase() === 'POST') {
-            if (!this.headers || !this.headers['Content-Type']) {
-                console.error('Content type is not specified for POST request.');
-                return;
-            }
-        }
-    
-        this.xhr.send(this.data);
-    },
-    
-    
-  
-    handleResponse: function() {
-      const responseHeaders = this.xhr.getAllResponseHeaders();
-  
-      if (typeof this.callback === 'function') {
+  handleResponse: function() {
+    const responseHeaders = this.xhr.getAllResponseHeaders();
+
+    if (this.targetElement) {
+     if (typeof this.callback === 'function') {
         this.callback.call(this.targetElement, responseHeaders);
-      } else if (this.callback === 'all') {
+     } else if (this.callback === 'all') {
         this.targetElement.innerHTML = responseHeaders;
-      } else if (this.callback === 'header' && this.headers && this.headers[this.headerName]) {
+     } else if (this.callback === 'header' && this.headers && this.headers[this.headerName]) {
         const specificHeader = this.xhr.getResponseHeader(this.headers[this.headerName]);
         this.targetElement.innerHTML = specificHeader || 'Header not found';
-      }
+     }
     }
-  };
+  }
+};
+
+// Expose grab to global scope
+window.grab = grab;
